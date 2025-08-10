8634b5bdbe1d03e6985add04724a54dd81450b40
YOO-uN-ee <jiyoonp0228@gmail.com>
2025-08-10 00:40:43 -0500

    add backend to settings

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,6 +13,6 @@ from django.core.asgi import get_asgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.backend.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = get_asgi_application()
